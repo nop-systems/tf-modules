226f0158ed7f541f7dbd9e84b4d22ffc015d11f6
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-04-12 07:12:52 +0200

    improve descriptions of variables

--- base/approle/variables.tf
+++ base/approle/variables.tf
@@ -1,6 +1,6 @@
 variable "fqdn" {
   type        = string
-  description = "The FQDN of the VM, will be used as role_name and for metadata"
+  description = "The hostname of the VM as FQDN, will be used as role_name and for metadata. Must be unique."
 }
 
 variable "cnames" {
@@ -15,7 +15,7 @@ variable "cnames" {
 
 variable "services" {
   type        = list(string)
-  description = "List of services to include in metdata (to access common secrets)"
+  description = "List of service FQDNs the host is instance of to include in metdata (to access common secrets)"
   default     = []
   validation {
     condition     = length(var.services) <= 5
@@ -31,5 +31,5 @@ variable "additional_token_policies" {
 
 variable "wrapping_ttl" {
   type        = string
-  description = "TTL for wrapped secret-id, unwrapped if set to null (duration string)"
+  description = "TTL for wrapped secret-id (duration string or null for unwrapped)"
 }
