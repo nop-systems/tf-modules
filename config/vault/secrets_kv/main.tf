bcd0216b290d440c832354990658d5db1489e20b
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-04-13 00:27:06 +0200

    change kv path and add service rules

--- config/vault/secrets_kv/main.tf
+++ config/vault/secrets_kv/main.tf
@@ -20,11 +20,37 @@ resource "vault_policy" "approle-kv-read" {
 
 data "vault_policy_document" "entity-metadata-kv-read" {
   rule {
-    path         = "kv/data/{{identity.entity.metadata.fqdn}}/*"
+    path         = "kv/data/host/{{identity.entity.metadata.fqdn}}/*"
     capabilities = ["read", "list"]
     description  = "Allow to read from kv path derived from entity metadata fqdn"
   }
+  rule {
+    path         = "kv/data/service/{{identity.entity.metadata.service00}}/*"
+    capabilities = ["read", "list"]
+    description  = "Allow to read from kv path derived from entity metadata service00"
+  }
+  rule {
+    path         = "kv/data/service/{{identity.entity.metadata.service01}}/*"
+    capabilities = ["read", "list"]
+    description  = "Allow to read from kv path derived from entity metadata service01"
+  }
+  rule {
+    path         = "kv/data/service/{{identity.entity.metadata.service02}}/*"
+    capabilities = ["read", "list"]
+    description  = "Allow to read from kv path derived from entity metadata service02"
+  }
+  rule {
+    path         = "kv/data/service/{{identity.entity.metadata.service03}}/*"
+    capabilities = ["read", "list"]
+    description  = "Allow to read from kv path derived from entity metadata service03"
+  }
+  rule {
+    path         = "kv/data/service/{{identity.entity.metadata.service04}}/*"
+    capabilities = ["read", "list"]
+    description  = "Allow to read from kv path derived from entity metadata service04"
+  }
 }
+
 
 resource "vault_policy" "entity-metadata-kv-read" {
   name   = "entity-metadata-kv-read"
