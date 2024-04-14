1c07cea922381ab2af153ed09b53ef22da39e66a
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-04-14 02:26:10 +0200

    stack: add missing wrapping_ttl for approle

--- base/fcos/stack/main.tf
+++ base/fcos/stack/main.tf
@@ -13,6 +13,7 @@ module "approle" {
   cnames                    = var.cnames
   services                  = var.services
   additional_token_policies = var.additional_token_policies
+  wrapping_ttl              = "10min"
 }
 
 module "ignition" {
