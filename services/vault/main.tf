ea2f64d0c7783c017d9a587a23ce5d7216497b2a
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-06-15 03:42:50 +0200

    add vault service

---
+++ services/vault/main.tf
@@ -0,0 +1,27 @@
+module "fcos" {
+  source = "git@github.com:nop-systems/tf-modules.git//base/fcos/stack?ref=fcos/v0.1.1"
+  #source = "../../base/fcos/stack"
+
+  fqdn      = var.fqdn
+  desc      = "HashiCorp Vault Node ${count.index + 1}"
+  xo_tags   = var.xo_tags
+  memory    = 4069
+  cpu_cores = 2
+  disk_size = 100
+
+  cloudflare_zone_id     = var.cloudflare_zone_id
+  xo_sr_id               = var.xo_sr_id
+  xo_network_id          = var.xo_network_id
+  xo_template_id         = var.xo_template_id
+  vault_url              = var.vault_url
+  root_ca_pem            = var.root_ca_pem
+  matchbox_http_endpoint = var.matchbox_http_endpoint
+
+  butane_snippets = [templatefile("${path.module}/vault.bu", {
+    fqdn                  = var.fqdn
+    ssh_authorized_keys   = jsonencode(var.ssh_authorized_keys)
+    cloudflare_zone_id    = var.cloudflare_zone_id
+    cluster_fqdn          = var.cluster_fqdn
+    cluster_members_fqdns = var.cluster_members_fqdns
+  })]
+}
---
+++ services/vault/variables.tf
@@ -0,0 +1,60 @@
+variable "fqdn" {
+  type        = string
+  description = "Fully Qualified Domain Name of VM"
+}
+
+variable "xo_network_id" {
+  type        = string
+  description = "XenOrchestra Network UUID"
+}
+
+variable "xo_sr_id" {
+  type        = string
+  description = "XenOrchestra Storage Repository UUID"
+}
+
+variable "xo_template_id" {
+  type        = string
+  description = "XenOrchestra Template UUID"
+}
+
+variable "xo_tags" {
+  type        = list(string)
+  description = "List of additional tags that should be attached to the VM"
+  default     = []
+}
+
+variable "vault_url" {
+  type        = string
+  description = "Vault URL"
+}
+
+variable "root_ca_pem" {
+  type        = string
+  description = "Private Root Certificate (PEM)"
+}
+
+variable "matchbox_http_endpoint" {
+  type        = string
+  description = "matchbox http endpoint"
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare Zone ID"
+  type        = string
+}
+
+variable "ssh_authorized_keys" {
+  description = "SSH authorized keys for VM"
+  type        = list(string)
+}
+
+variable "cluster_members_fqdns" {
+  description = "FQDNs of all cluster members"
+  type        = list(string)
+}
+
+variable "cluster_fqdn" {
+  description = "round-robin fqdn"
+  type        = string
+}
