1177e869dcf2b1fac32015e86dbb16f0ae218474
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-06-16 10:24:38 +0200

    bump fcos

--- services/vault/main.tf
+++ services/vault/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "fcos" {
-  source = "git@github.com:nop-systems/tf-modules.git//base/fcos/stack?ref=fcos/v0.2.2"
+  source = "git@github.com:nop-systems/tf-modules.git//base/fcos/stack?ref=fcos/v0.2.3"
   #source = "../../base/fcos/stack"
 
   fqdn      = var.fqdn
