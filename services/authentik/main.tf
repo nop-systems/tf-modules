27c22d429082d1136d1853fc46d273fc62181ef3
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-06-18 03:39:44 +0200

    authentik: first draft

---
+++ services/authentik/main.tf
@@ -0,0 +1,25 @@
+module "fcos" {
+  source = "git@github.com:nop-systems/tf-modules.git//base/fcos/stack?ref=fcos/v0.2.3"
+  # source = "../../base/fcos/stack"
+
+  fqdn      = var.fqdn
+  desc      = "authentik"
+  xo_tags   = concat(var.xo_tags, ["authentik"])
+  memory    = 2048
+  cpu_cores = 2
+  disk_size = 10
+
+  butane_snippets = [templatefile("${path.module}/authentik.bu", {
+    service             = var.service_fqdn
+    trusted_proxy_cidrs = var.trusted_proxy_cidrs
+  })]
+
+  cloudflare_zone_id     = var.cloudflare_zone_id
+  xo_sr_id               = var.xo_sr_id
+  xo_network_id          = var.xo_network_id
+  xo_template_id         = var.xo_template_id
+  vault_url              = var.vault_url
+  root_ca_pem            = var.root_ca_pem
+  matchbox_http_endpoint = var.matchbox_http_endpoint
+}
+
