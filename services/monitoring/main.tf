4308531ed4611d2d687689482fcb6386009d2b36
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2024-07-16 07:23:52 +0200

    monitoring: bump fcos, remove now redundant pki-admin vault agent, fix SNIs

--- services/monitoring/main.tf
+++ services/monitoring/main.tf
@@ -8,7 +8,7 @@ locals {
 }
 
 module "fcos" {
-  source = "git@github.com:nop-systems/tf-modules.git//base/fcos/stack?ref=fcos/v0.4.0"
+  source = "git@github.com:nop-systems/tf-modules.git//base/fcos/stack?ref=fcos/v0.5.0"
 
   fqdn      = var.fqdn
   desc      = "Monitoring Stack"
